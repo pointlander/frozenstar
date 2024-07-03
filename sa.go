88042cf4432238caea01d15695e7d88667fbba36
Andrew Snodgrass <pointlander@gmail.com>
2024-07-03 17:21:29 -0500

    Added self attention stub

---
+++ sa.go
@@ -0,0 +1,10 @@
+// Copyright 2024 The FrozenStar Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package main
+
+// SA is self attention mode
+func SA() {
+
+}
